cb7967baac5b3a89df48d73d7fab031e5daae33c
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2024-02-15 19:55:16 -0800

    Beta AcousticMobilePush build: 3.9.5
    Beta AcousticMobilePush Change Notes:
    Bug Fixes
    
    - None
    
    Improvements:
    
    - Update for xcode 15.x.
    - Add to cocoapods public site.
    - Add Carthage support.
    - Add swift packages support.
    
    Our environment for this release:
    
    - Xcode 15.0.1
    - MacOS 14.1.2
    - iOS 12.x to 17.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    	- watch
    		- arm64
    		- arm64_32
    		- armv7k
    		- i386
    		- x86_64
    	- macos
    		- arm64
    		- x86_64-maccatalyst
    		- x86_64-simulator
    
    Known Issues
    
    - ?

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "AcousticMobilePushWatch",
-            url: "https://github.com/Acoustic-Mobile-Push/iOS/releases/download/3.9.4/AcousticMobilePushWatch_XCFramework_Debug.zip",
-            checksum: "6845ad6785052f6d3fc44b2da2c000e99b42dea9dd7e839317b4ff948d220594"),
+            url: "https://github.com/Acoustic-Mobile-Push/iOS/releases/download/3.9.5/AcousticMobilePushWatch_XCFramework_Debug.zip",
+            checksum: "fbfc0058d3c33249f0863ea0bf7d5d7e0bfb2682098156d5f8e04068237631a9"),
     ]
 )
